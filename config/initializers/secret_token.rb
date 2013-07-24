ade4c6bd9f848eec8b2343e60b88cab02a3a0e13
JD <johndavid400@gmail.com>
2013-07-24 10:39:43 -0500

    add new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyPics::Application.config.secret_token = 'b75087372e601d24ae4923100d32b29d7ad1119294cdf8fad4ce277e0c7e42b08ddbb0490fd5397227d30f98581603b232a9ad51df6a7e3d29d67eb55babaa98'
